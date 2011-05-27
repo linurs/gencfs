21dafcee84fa0eaff2b0ac5e6e876222e7b4325d
Urs Lindegger <lindegur@black.(none)>
2011-05-27 19:39:00 +0200

    porting to python 3

--- gencfs.py
+++ gencfs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 ## @package gencfs 
 # @author urs lindegger urs@linurs.org  
@@ -9,6 +9,10 @@
 # Bug fixes and improvements from previous version:
 # ---------------------------------------------------
 # 
+# From Version 0.2 to 0.3
+# -----------------------
+# - porting to python3
+#
 # From Version 0.1 to 0.2
 # -----------------------
 # - popen4 does not work anymore since it is deprecated and replaced by subprocess module 
@@ -22,8 +26,8 @@
 import os
 import subprocess
 import shlex
-import tkMessageBox 
-import Tkinter 
+import tkinter.messagebox 
+import tkinter 
 
 mountflag=0 
 pathtoencfs=""
@@ -35,19 +39,21 @@ def encfsmount():
  pathtoencfs=window.pathencfs.get()
  cmd ="encfs -S "+pathtoencfs+"/.crypt "+pathtoencfs+"/crypt" # call encfs without promting for password
  args=shlex.split(cmd)
+ 
  p = subprocess.Popen(args,  
                       stdin=subprocess.PIPE, 
                       stdout=subprocess.PIPE,
                       stderr=subprocess.STDOUT, # standard err are passed to stdout
                       ) # open new process
- stdout_value, stderr_value = p.communicate(password) # communicate is a one time action, afterwards p is closed
- if (stdout_value==""):
-  tkMessageBox.showinfo("mount","Successfully mounted")
+ b=password.encode('utf-8')
+ stdout_value, stderr_value = p.communicate(b) # communicate is a one time action, afterwards p is closed
+ if (stdout_value==b''):
+  tkinter.messagebox.showinfo("mount","Successfully mounted")
   mountflag=1
-  window.mountbutton.config(relief=Tkinter.SUNKEN)
-  window.umountbutton.config(relief=Tkinter.RAISED)
+  window.mountbutton.config(relief=tkinter.SUNKEN)
+  window.umountbutton.config(relief=tkinter.RAISED)
  else: 
-  tkMessageBox.showinfo("mount",stdout_value)
+  tkinter.messagebox.showinfo("mount",stdout_value)
  return 0  
 
 def encfsumount():
@@ -62,13 +68,13 @@ def encfsumount():
                       stderr=subprocess.STDOUT, # standard err are passed to stdout
                       ) # open new process
  stdout_value, stderr_value = p.communicate()# communicate is a one time action, afterwards p is closed
- if (stdout_value==""):
-  tkMessageBox.showinfo("umount","Successfully umounted")
+ if (stdout_value==b''):
+  tkinter.messagebox.showinfo("umount","Successfully umounted")
   mountflag=0
-  window.umountbutton.config(relief=Tkinter.SUNKEN)
-  window.mountbutton.config(relief=Tkinter.RAISED)
- else: 
-  tkMessageBox.showinfo("umount",stdout_value)
+  window.umountbutton.config(relief=tkinter.SUNKEN)
+  window.mountbutton.config(relief=tkinter.RAISED)
+ else:  
+  tkinter.messagebox.showinfo("umount",stdout_value)
  return 0
 
 ## Main program, get persistent data as path to encfs 
@@ -77,35 +83,35 @@ def encfsumount():
 userpath=os.path.expanduser("~")          # check what user
 configdir=userpath+"/.GEncFs"
 if os.access(configdir, os.F_OK)==False:  # check if user has a directory containing persistent data
-  os.mkdir(configdir,0777)                # if not create the directory
+  os.mkdir(configdir,0o777)                # if not create the directory
 configfile="conf"                            
 pathtoconfig=configdir+"/"+configfile
 if os.access(pathtoconfig, os.F_OK)==False:   # check if file exists containing persistent data
 #  os.chdir(configdir)
   os.system("touch "+pathtoconfig) # if not create the empty file
-pathtoconfigfile=file(pathtoconfig,'r') # now read the file containing persistent data or being empty
+pathtoconfigfile=open(pathtoconfig) # now read the file containing persistent data or being empty
 pathtoencfscr=pathtoconfigfile.readline()
 pathtoencfs=pathtoencfscr.strip('\n')
 pathtoconfigfile.close()
  
-window=Tkinter.Tk()
+window=tkinter.Tk()
 window.title('Gui for EncFs')
-window.label=Tkinter.Label(window, text='Password')
+window.label=tkinter.Label(window, text='Password')
 window.label.pack()
 
-window.password=Tkinter.Entry(window, show='*')
+window.password=tkinter.Entry(window, show='*')
 window.password.pack()
 
-window.mountbutton=Tkinter.Button(master=window,text='Mount EncFs',command=encfsmount)
+window.mountbutton=tkinter.Button(master=window,text='Mount EncFs',command=encfsmount)
 window.mountbutton.pack()
 
-window.umountbutton=Tkinter.Button(master=window,text='Umount EncFs',relief=Tkinter.SUNKEN, command=encfsumount)
+window.umountbutton=tkinter.Button(master=window,text='Umount EncFs',relief=tkinter.SUNKEN, command=encfsumount)
 window.umountbutton.pack()
 
-window.labelpath=Tkinter.Label(window, text='Path to the .crypt and crypt directories')
+window.labelpath=tkinter.Label(window, text='Path to the .crypt and crypt directories')
 window.labelpath.pack()
 
-window.pathencfs=Tkinter.Entry(window, width=50)
+window.pathencfs=tkinter.Entry(window, width=50)
 window.pathencfs.pack()
 window.pathencfs.insert(0, pathtoencfs)
 
