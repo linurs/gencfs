6148b2e3e87ca9a3e8f9840a92ff6e1376aabb55
Urs Lindegger <urs@linurs.org>
2018-10-08 16:38:14 +0200

    ready to test and distribute

--- gencfs.py
+++ gencfs.py
@@ -5,17 +5,8 @@
 
 ## @todo 
 # - license
-# - pyinstaller version
-# - check if mounted before mounting cat /proc/mounts
-# - use tab insted of new window or withdraw() Removes the window from the screen (without destroying it). To redraw the window, use deiconify. 
-#       When the window has been withdrawn, the state method returns “withdrawn”. Top window can be more than once 
-# - use event handlers 
-# - doxygen
-# - block top window when child windows are open
-# - support checkfiles other than .encfs6.xml or just to see if files are there
-# - add verion number
-# - 0o777 is ok?
-# - why to read from stderr
+# - make clearer what directory is selected
+# - check if encfs is installed (whereis?)
 #
 
 ## @mainpage gencfs
@@ -31,50 +22,51 @@ import sys
 
 try:
    import pexpect
-   expect=True
+## indicates that pexpect is available
+   expect=True 
 except:
    expect=False
    
 from tkinter import *   
-#from tkinter import ttk  
 from tkinter import messagebox   
-from tkinter import filedialog   
+from tkinter import filedialog    
 
-gencfsversion="0.4"
-encfsxml=".encfs6.xml" 
+## Version of gencfs
+gencfsversion="0.5"
+
+## favicon file to seen in window decoration
 faviconname='favicon.gif'
 
 class app_t():
 ##
 # The constructor for the GUI application   
     def __init__(self):
-        userpath=os.path.expanduser("~")          # check what user
+        userpath=os.path.expanduser("~")           # check what user
         configdir=userpath+"/.GEncFs"
         if os.access(configdir, os.F_OK)==False:  # check if user has a directory containing persistent data
-          os.mkdir(configdir,0o777)                         # if not create the directory
-        configfile="conf"                            
+          os.mkdir(configdir)                                    # if not create the directory
+        configfile="conf"
+        ## path to config file         
         self.pathtoconfig=configdir+"/"+configfile
+        logging.debug("Config file "+self.pathtoconfig)
         if os.access(self.pathtoconfig, os.F_OK)==False:   # check if file exists containing persistent data
           os.system("touch "+self.pathtoconfig)               # if not create the empty file
-          self.claim_filecontent(self.pathtoconfig)
         pathtoconfigfile=open(self.pathtoconfig) # now read the file containing persistent data or being empty
         pathstoencfs=pathtoconfigfile.readlines()  
         pathtoconfigfile.close()
-        if len(pathstoencfs)==0:
-            self.claim_filecontent(self.pathtoconfig)
         
-        # the list box does not point to anything the last position is taken
+        ## the list box does not point to anything the last position is taken
         self.index='0'
         
-        # setup the gui stuff
+        ## setup the gui stuff
         self.window=Tk()
         self.window.title('Gui for EncFs')
         
         # add an icon
         img = PhotoImage(file=favicon)
-        self.window.call('wm', 'iconphoto', self.window._w, img)
+        self.window.call("wm", "iconphoto", self.window, "-default", img)
         
-        # create the menus   
+        ## create the menus   
         self.menubar = Menu(self.window)
         filemenu = Menu(self.menubar, tearoff=0)
         filemenu.add_command(label="Add directory for .crypt and crypt", command=self.add)
@@ -111,22 +103,28 @@ class app_t():
             self.window.listbox.insert(END, i)
         self.window.listbox.select_set(0)  # selects the first one, so something is selected    
         
-        self.window.mainloop()
+        self.window.mainloop()      
         
-    def claim_filecontent(self, pathtoconfig):
-         messagebox.showerror("Error","Configfile is empty. Add path to "+pathtoconfig+ " for .crypt and crypt")       
-         self.window.destroy()
+###
+## Error config file not found
+#    def claim_filecontent(self, pathtoconfig):
+#         messagebox.showerror("Error","Configfile is empty. Add path to "+pathtoconfig+ " for .crypt and crypt")       
+#         self.window.destroy()
         
+##
+# Quits the application
     def quit(self):
            self.window.destroy()
-           
+##
+# updates the config file by re-creating it
     def updateconfig(self):
         items=self.window.listbox.get(0, END)
         pathtoconfigfile=open(self.pathtoconfig,  'w') # now write the file containing persistent data
         for i in items:
             pathtoconfigfile.write(i+"\n")  
         pathtoconfigfile.close()
-        
+##
+# Check if directory exists alerady
     def existconfig(self, n):
         items=self.window.listbox.get(0, END)
         value=False
@@ -134,7 +132,9 @@ class app_t():
             if i==n:
                 value=True
         return value        
- 
+
+##
+# Adds new directory to the list
     def add(self):
         dname = filedialog.askdirectory(initialdir='~')   
         if self.existconfig(dname)==True:
@@ -145,14 +145,19 @@ class app_t():
                 os.mkdir(dname)
             self.window.listbox.insert(END, dname)
             self.updateconfig()  
-         
+##
+# Removes directory from the list
     def remove(self):
         self.window.listbox.delete(ANCHOR)
         self.updateconfig()
-           
+
+##
+# Shows abbout messagebox
     def about(self):
-         messagebox.showinfo("About","Gencfs a gui front end for Encfs from https://www.linurs.org")       
-           
+         messagebox.showinfo("About","Gencfs a gui front end for Encfs from https://www.linurs.org \nVersion "+gencfsversion)       
+
+##
+# Mounts the encripted directory
     def encfsumount(self):
          i=self.window.listbox.curselection()
          if len(i)>0:
@@ -171,30 +176,38 @@ class app_t():
          else:  
           messagebox.showinfo("umount",stdout_value)
          return 0
-           
+
+##
+# Un-mounts the encripted directory           
     def encfsmount(self):
          i=self.window.listbox.curselection()
          if len(i)>0:
           self.index=i[0]
          pathtoencfs=self.window.listbox.get(self.index)
-         password=self.window.password.get()
-         b=password.encode('utf-8')
-         cmd ="encfs --stdinpass "+pathtoencfs+"/.crypt "+pathtoencfs+"/crypt" # call encfs without promting for password
-         args=shlex.split(cmd)
-         
-         p = subprocess.Popen(args,  
-                              stdin=subprocess.PIPE, 
-                              stdout=subprocess.PIPE,
-                              stderr=subprocess.STDOUT, # standard err are passed to stdout
-                              ) # open new process
-        
-         stdout_value, stderr_value = p.communicate(b) # communicate is a one time action, afterwards p is closed
-         if (stdout_value==b''):
-          messagebox.showinfo("mount","Successfully mounted")
-         else: 
-          messagebox.showinfo("mount",stdout_value)
-         return 0     
+         mounted=os.path.ismount(pathtoencfs+"/crypt")
+         if mounted==False:
+             password=self.window.password.get()
+             b=password.encode('utf-8')
+             cmd ="encfs --stdinpass "+pathtoencfs+"/.crypt "+pathtoencfs+"/crypt" # call encfs without promting for password
+             args=shlex.split(cmd)
+             
+             p = subprocess.Popen(args,  
+                                  stdin=subprocess.PIPE, 
+                                  stdout=subprocess.PIPE,
+                                  stderr=subprocess.STDOUT, # standard err are passed to stdout
+                                  ) # open new process
+            
+             stdout_value, stderr_value = p.communicate(b) # communicate is a one time action, afterwards p is closed
+             if (stdout_value==b''):
+              messagebox.showinfo("mount","Successfully mounted")
+             else: 
+              messagebox.showinfo("mount",stdout_value)
+             return 0   
+         else:
+             messagebox.showinfo("mount",pathtoencfs+"/crypt is already mounted")
 
+##
+# creates .crypt and crypt directories
     def create(self):
          i=self.window.listbox.curselection()
          if len(i)>0:
@@ -203,17 +216,19 @@ class app_t():
          if os.path.isdir(pathtoencfs)==False:
                messagebox.showinfo("Error","Directory "+pathtoencfs+" not found")
          else:      
+             ## path to encripted directory
              self.dcrypt=pathtoencfs+"/.crypt"
+             ## mounting point for encripted directory (decrypted directory)
              self.crypt=pathtoencfs+"/crypt"
              if os.path.isdir(self.crypt)==False:
                     messagebox.showinfo("Info","Mounting point "+self.crypt+" will be created")       
                     os.mkdir(self.crypt) 
              if os.path.isdir(self.dcrypt)==True:
                     messagebox.showinfo("Info","Crypted directory "+self.dcrypt+" already exist")
-                    xmlfile=self.dcrypt+"/"+encfsxml
-                    if os.path.isfile(xmlfile)==True:  
+                    if len(os.listdir(self.dcrypt))>0:
                         messagebox.showinfo("Error","Crypted directory is not empty")             
                     else:
+                        ## indication if new password has to be entered or existing password has to be changed
                         self.changepassword=False
                         self.newpassword()        
              else:
@@ -222,46 +237,61 @@ class app_t():
                     self.changepassword=False
                     self.newpassword()          
 
+##
+# destroys the password window and re-shows the main window
+    def destroypasswordwindow(self):
+            self.window.deiconify()
+            self.newpasswordwindow.destroy()
+
+##
+# creates the password window that is used to enter a password for newly created directories but also to change the password for existing directories.
     def newpassword(self):
-           w=self.window.winfo_children() 
-           childwindow=False
-           for x in w:
-               if x.widgetName=="toplevel":
-                   childwindow=True
-           if childwindow==False: # prevents that multiple child windows can open
-           
-               self.newpasswordwindow=Toplevel(master=self.window)
-               self.newpasswordwindow.title('Password')
+        if (expect==True) or (self.changepassword==False):
+            self.window.withdraw()
+            
+            ## window to set the password      
+            self.newpasswordwindow=Toplevel()
+            self.newpasswordwindow.protocol("WM_DELETE_WINDOW", self.destroypasswordwindow)
+            self.newpasswordwindow.title('Password')
     
-               self.askpasswordframe=LabelFrame(master=self.newpasswordwindow,
-                                                 text="New Password")
-               self.askpasswordframe.grid(column=0,  row=0)
+            ## setup password entry variable
+            self.newpassword1=StringVar()
+            self.window.passlabel1=Label(self.newpasswordwindow, text='New Password', width=20,  anchor=W)
+            self.window.passlabel1.grid(column=0,  row=0, sticky= W)
+            ## setup password entry        
+            self.pass1=Entry(master=self.newpasswordwindow,
+                                       textvariable=self.newpassword1, show='*',     width=40 )
+            self.pass1.grid(column=1,  row=0)
+            
+            ## setup password entry variable for confirmation
+            self.newpassword2=StringVar()
+            self.window.passlabel2=Label(self.newpasswordwindow, text='Verify Password', width=20,  anchor=W)
+            self.window.passlabel2.grid(column=0,  row=1, sticky= W)
+            ## setup password entry for confirmation
+            self.pass2=Entry(master=self.newpasswordwindow,
+                                       textvariable=self.newpassword2, show='*',     width=40 )
+            self.pass2.grid(column=1,  row=1)
     
-             # the passwords
-               self.newpassword1=StringVar()
-               self.askportname=Entry(master=self.askpasswordframe,
-                                           textvariable=self.newpassword1, show='*')
-               self.askportname.grid(column=0,  row=0)
-               self.newpassword2=StringVar()
-               self.askportname=Entry(master=self.askpasswordframe,
-                                           textvariable=self.newpassword2, show='*')
-               self.askportname.grid(column=0,  row=1)
-    
-               # The ok button
-               self.ok_askportbutton=Button(master=self.newpasswordwindow,
-                                                  text='OK',
-                                                  command=self.okpassword)
-               self.ok_askportbutton.grid(column=0,
+           ## setup the ok button
+            self.okbutton=Button(master=self.newpasswordwindow,
+                                              text='OK',
+                                              width=20, 
+                                              command=self.okpassword)
+            self.okbutton.grid(column=1,
+                                         row=2,
+                                         sticky=W)
+           ## setup the cancel button
+            self.cancelbutton=Button(master=self.newpasswordwindow,
+                                              text='CANCEL',
+                                              width=20, 
+                                              command=self.cancelpassword)
+            self.cancelbutton.grid(column=0,
                                              row=2,
                                              sticky=W)
-               # The cancel button
-               self.cancel_askportbutton=Button(master=self.newpasswordwindow,
-                                                  text='CANCEL',
-                                                  command=self.cancelpassword)
-               self.cancel_askportbutton.grid(column=0,
-                                                 row=3,
-                                                 sticky=W)
-    
+        elif (expect==False):
+                 messagebox.showinfo("Error","pexpect needs to be installed for the password change. Install it. See: https://pexpect.readthedocs.io \n")                                      
+##
+# Ok button pressed in password window
     def okpassword(self):
         self.newpasswordwindow.destroy()
         p1=self.newpassword1.get()+'\n'
@@ -280,13 +310,11 @@ class app_t():
                               ) # open new process
            
             stdout_value, stderr_value = p.communicate(newb) # communicate is a one time action, afterwards p is closed
-            xmlfile=self.dcrypt+"/"+encfsxml
-            if os.path.isfile(xmlfile)==True:  
+            if len(os.listdir(self.dcrypt))>0:
                     messagebox.showinfo("Info","Successfully created\n"+stdout_value.decode("utf-8"))
             else:
                     messagebox.showinfo("Error","Failed to create\n"+stdout_value.decode("utf-8") )       
         else:   # change password on existing directory       
-            if expect==True:
                  # make sure it is unmounted 
                  cmd ="fusermount -u "+self.crypt
                  args=shlex.split(cmd)
@@ -301,29 +329,33 @@ class app_t():
                  b=password.encode('utf-8')
                  child = pexpect.spawn("encfsctl passwd "+self.dcrypt)
                  try:
-                    child.expect("EncFS Password: ", timeout=2)
+                    child.expect("EncFS Password:", timeout=2)
                  except:
                        messagebox.showinfo("Error",str(child))
                  child.sendline(b)             
                  try:
-                    child.expect("New Encfs Password: ", timeout=2)
+                    child.expect("New Encfs Password:", timeout=2)
                  except:
                     pass
                  if child.terminated==False:
                      child.sendline(p1)
                      try:
-                            child.expect("Verify Encfs Password: ", timeout=2)
+                            child.expect("Verify Encfs Password:", timeout=2)
                      except:
                            messagebox.showinfo("Error",str(child))    
                      child.sendline(p2)
                  before=child.before
                  messagebox.showinfo("Info",before.decode("utf-8"))   
-            else:
-                 messagebox.showinfo("Error","pexpect is not installed but required for password change. https://pexpect.readthedocs.io \n") 
+        self.window.deiconify()            
                          
+##
+# Cancel button pressed in password window
     def cancelpassword(self):
         self.newpasswordwindow.destroy()
-         
+        self.window.deiconify()
+
+##
+# change the password of an existing directory
     def passwd(self):
          i=self.window.listbox.curselection()
          if len(i)>0:
@@ -340,18 +372,23 @@ if __name__ == "__main__":
     # sets default values to variables and modifies their content according the command line parameter passed
     # additionally it handles the -h and --help command line parameter automatically
     parser = argparse.ArgumentParser(
-                                     description='modme - A modbus py application', 
+                                     description='gencfs - A gui for encfs', 
                                      epilog='urs@linurs.org')
-    parser.add_argument('-v', '--version', action='version', version='%(prog)s '+gencfsversion)
+    ## command line option to show the programs version
+    parser.add_argument('-v', '--version', action='version', \
+    ## version used in command line option to show the programs version
+    version='%(prog)s '+gencfsversion)
+    ## command line option to enable debug messages
     parser.add_argument('-d', '--debug',   help="print debug messages",   action='store_true')  
 
-    # the command line arguments passed
+    ## the command line arguments passed
     args = parser.parse_args()      
     
     # Configuring the logger. Levels are DEBUG, INFO, WARNING, ERROR and CRITICAL
     # the parameter filename='example.log' would write it into a file
     logging.basicConfig() # init logging 
-    logger = logging.getLogger() # get the root logger 
+    ## The root logger 
+    logger = logging.getLogger() 
     if args.debug==True:
         logger.setLevel(logging.DEBUG)    # the level producing debug messagesl
     else:    
@@ -362,11 +399,12 @@ if __name__ == "__main__":
     if expect==False:
          logger.debug('Packet pexpectl not found')
     
-    # pyinstaller stuff required to create bundled versions as for windows exe:      
+    ## pyinstaller stuff required to create bundled versions:      
     frozen = 'not '
     if getattr(sys, 'frozen', False): # pyinstaller adds the name frozen to sys 
             frozen = ''  # we are running in a bundle (frozen)
-            bundle_dir = sys._MEIPASS  # temporary folder of pyinstaller
+            ## temporary folder of pyinstaller
+            bundle_dir = sys._MEIPASS  
     else:
             bundle_dir = os.path.dirname(os.path.abspath(__file__))   # we are running in a normal Python environment 
    
@@ -376,7 +414,7 @@ if __name__ == "__main__":
     logging.debug('sys.executable is '+sys.executable )
     logging.debug('os.getcwd is '+os.getcwd() )
 
-# makes that the files are found
+## makes that the files are found
     favicon=bundle_dir+os.sep+faviconname    
-    
+## start the application    
     app=app_t()
